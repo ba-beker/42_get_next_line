249b71b5504b9d7cca4b5201a2fcb8edcd68e614
mobabeke <mobabeke@student.42wolfsburg.de>
2023-01-25 12:34:36 +0100

    trying the new envirement

---
+++ get_next_line.c
@@ -0,0 +1,20 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<fcntl.h>
+char *get_next_line()
+{
+	int fd;
+	char *msg = "Hi there!";
+	char buffer[80];
+	fd = open("index.txt", O_RDWR);
+	printf("%d",fd);
+	write(fd, msg, sizeof(msg));
+	read(fd, buffer, sizeof(msg));
+	printf("%s", buffer);
+	close(fd);
+
+}
+int main()
+{
+	get_next_line();
+}
